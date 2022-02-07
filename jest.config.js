38608c25442c2e705bcb2947db4d16ebe468c5ee
Samuel Lee <sam@samlee.dev>
2022-02-07 01:06:42 -0600

    build scripts

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,3 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-};
+export const preset = 'ts-jest';
+export const testEnvironment = 'node';
