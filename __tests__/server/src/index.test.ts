7e21e5f11cd3c7c479527295fc7deaa3d140519a
Samuel Lee <sam@samlee.dev>
2022-02-07 00:32:43 -0600

    linter and formatter

--- __tests__/server/src/index.test.ts
+++ __tests__/server/src/index.test.ts
@@ -1,23 +1,23 @@
-import * as index from "../../../server/src/index";
+import * as index from '../../../server/src/index';
 // @ponicode
-describe("index.func", () => {
-  test("0", () => {
-    let result: any = index.func("foo bar");
+describe('index.func', () => {
+  test('0', () => {
+    const result: any = index.func('foo bar');
     expect(result).toMatchSnapshot();
   });
 
-  test("1", () => {
-    let result: any = index.func("Foo bar");
+  test('1', () => {
+    const result: any = index.func('Foo bar');
     expect(result).toMatchSnapshot();
   });
 
-  test("2", () => {
-    let result: any = index.func("Hello, world!");
+  test('2', () => {
+    const result: any = index.func('Hello, world!');
     expect(result).toMatchSnapshot();
   });
 
-  test("3", () => {
-    let result: any = index.func("");
+  test('3', () => {
+    const result: any = index.func('');
     expect(result).toMatchSnapshot();
   });
 });
