a3c607df250ced44c5cdaba521f446f779132c4e
Samuel Lee <sam@samlee.dev>
2022-02-07 00:16:42 -0600

    refactor repo, run tests at top level

--- __tests__/server/src/index.test.ts
+++ __tests__/server/src/index.test.ts
@@ -1,27 +1,22 @@
-import * as index from "./index";
+import * as index from "../../../server/src/index";
 // @ponicode
 describe("index.func", () => {
   test("0", () => {
-    let result: any = index.func("Foo bar");
-    expect(result).toMatchSnapshot();
-  });
-
-  test("1", () => {
     let result: any = index.func("foo bar");
     expect(result).toMatchSnapshot();
   });
 
-  test("2", () => {
-    let result: any = index.func("This is a Text");
+  test("1", () => {
+    let result: any = index.func("Foo bar");
     expect(result).toMatchSnapshot();
   });
 
-  test("3", () => {
+  test("2", () => {
     let result: any = index.func("Hello, world!");
     expect(result).toMatchSnapshot();
   });
 
-  test("4", () => {
+  test("3", () => {
     let result: any = index.func("");
     expect(result).toMatchSnapshot();
   });
