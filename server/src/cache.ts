56a24c7afc8d5afb0b22f442a25df1dd3546c0b4
Lu Cao <lu.cao@vanderbilt.edu>
2022-02-21 15:43:35 -0600

    Refactor to use singleton redis client

--- server/src/cache.ts
+++ server/src/cache.ts
@@ -1,37 +1,16 @@
-import * as redis from 'redis';
-import dotenv from 'dotenv';
-
-dotenv.config({
-  path: `${__dirname}/../../.env`
-});
-
-const client = redis.createClient({
-  url: process.env.REDIS_URL,
-  socket: {
-    tls: true,
-    rejectUnauthorized: false
-  }
-});
-
-client.on('error', (err) => {
-  console.log('Error ' + err);
-});
+import { client } from './init_redis';
 
 export const insertData = async (data: any, db: number) => {
-  await client.connect();
   await client.select(db);
   const diningHallName = data.diningHallName;
   await client.rPush(diningHallName, JSON.stringify(data));
-  await client.disconnect();
 };
 
 export const getDataForDiningHall = async (
   diningHallName: string,
   db: number
 ) => {
-  await client.connect();
   await client.select(db);
   const result = await client.lRange(diningHallName, 0, -1);
-  await client.disconnect();
   return result;
 };
