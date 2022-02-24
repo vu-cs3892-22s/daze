d9a253f4ac78b236545059d0b525b148c9db90fb
Sophia Chen <sophiachen@Sophias-MacBook-Pro.local>
2022-02-24 13:30:05 -0600

    use req.body

--- server/src/db.ts
+++ server/src/db.ts
@@ -1,6 +1,5 @@
 import { Pool } from 'node-postgres';
 import dotenv from 'dotenv';
-import { resolve } from 'path/posix';
 
 dotenv.config()
 
@@ -15,69 +14,63 @@ const pool = new Pool({
 
 pool.on('error', (err, client) => {
   console.error('Unexpected error on idle client', err);
-  //process.exit(-1);
 })
 
-
-export async function queryCreateUser(body: any){
+export async function queryCreateUser(body: any) {
   const text = `
     INSERT INTO users ("VanderbiltEmail", "FirstName", "LastName", "PhoneNumber")
     VALUES ('${body.VanderbiltEmail}', '${body.FirstName}', '${body.LastName}', '${body.PhoneNumber}')
     RETURNING "VanderbiltEmail"
   `;
-  
+
   const client = await pool.connect();
 
   try {
     const res = await client.query(text);
-    //console.log(res.rows[0]);
+
     client.release();
     return true;
   } catch (err: any) {
     client.release();
-    //console.log(err.stack);
+
     return false;
   }
-} 
+}
 
-export async function queryUpdateUser(body: any){
+export async function queryUpdateUser(body: any) {
   const text = `
     UPDATE users 
     SET "FirstName" = '${body.FirstName}', "LastName" = '${body.LastName}', "PhoneNumber" = '${body.PhoneNumber}'
     WHERE "VanderbiltEmail" = '${body.VanderbiltEmail}'
   `;
-  
+
   const client = await pool.connect();
 
   try {
     const res = await client.query(text);
-    //console.log(res.rows[0])
     client.release();
     return true;
   } catch (err: any) {
     client.release();
-    //console.log(err.stack)
     return false;
   }
 }
 
-export async function queryGetUser(email: any){
+export async function queryGetUser(email: any) {
   const text = `
     SELECT * 
     FROM users
     WHERE "VanderbiltEmail" = '${email}'
   `;
-  
+
   const client = await pool.connect();
 
   try {
     const res = await client.query(text);
-    //console.log(res.rows[0])
     client.release();
     return true;
   } catch (err: any) {
     client.release();
-    //console.log(err.stack)
     return false;
   }
 }
--- server/src/handlers.ts
+++ server/src/handlers.ts
@@ -2,23 +2,22 @@ import express from 'express';
 
 //import { pool } from './db';
 
-import { queryCreateUser, 
-        queryUpdateUser, 
-        queryGetUser} from './db'
+import { queryCreateUser, queryUpdateUser, queryGetUser } from './db';
 
-let test  = {
-  "VanderbiltEmail": "sophia@gmail.com", 
-  "FirstName": "Sop",
-  "LastName": "Chen",
-  "PhoneNumber": "12345"
-}
+let test = {
+  VanderbiltEmail: 'sophia@gmail.com',
+  FirstName: 'Sop',
+  LastName: 'Chen',
+  PhoneNumber: '12345'
+};
 
-export const createUser = async (req: express.Request, res: express.Response) => {
-  console.log("This is the req.body: " + JSON.stringify(req.body));
-  let success = await queryCreateUser(test);
-  //console.log("add user returns this: " + success)
+export const createUser = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  let success = await queryCreateUser(req.body);
 
-  if (success === true){
+  if (success === true) {
     res.status(201).send({
       message: 'Creating user: Success'
     });
@@ -29,10 +28,13 @@ export const createUser = async (req: express.Request, res: express.Response) =>
   }
 };
 
-export const updateUser = async (req: express.Request, res: express.Response) => {
-  let success = await queryUpdateUser(test);
+export const updateUser = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  let success = await queryUpdateUser(req.body);
 
-  if (success === true){
+  if (success === true) {
     res.status(201).send({
       message: 'Updating user: Success'
     });
@@ -46,9 +48,9 @@ export const updateUser = async (req: express.Request, res: express.Response) =>
 export const getUser = async (req: express.Request, res: express.Response) => {
   const vunetId = req.params.vunet_id;
 
-  let success = await queryGetUser(test.VanderbiltEmail);
+  let success = await queryGetUser(vunetId);
 
-  if (success === true){
+  if (success === true) {
     res.status(200).send({
       message: 'Getting user: Success'
     });
