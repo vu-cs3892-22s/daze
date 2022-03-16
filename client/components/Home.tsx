6a7e36dc78eae3c10da389f8131ce1a9225bcd1f
Chinchuluun1029 <chinchuluun1029@gmail.com>
2022-03-16 10:27:36 -0500

    Uncomment few locations, move styles to bottom

--- client/components/Home.tsx
+++ client/components/Home.tsx
@@ -4,6 +4,58 @@ import { Box } from "native-base";
 
 import Card from "./Card";
 
+interface HomeScreenProps {
+  navigation?: Object;
+}
+
+export default function HomeScreen({ navigation }: HomeScreenProps) {
+  return (
+    <View style={styles.baseView}>
+      <ScrollView style={styles.scrollView}>
+        <Box style={styles.drawerContainer}>
+          <Box style={styles.drawerBox}>
+            <Card
+              name={"2301 Allergen Free"}
+              idx={0}
+              line="s"
+              data={[20, 35, 75, 10, 45]}
+              img={"https://i.ibb.co/p4kMnkZ/2301.png"}
+            />
+            <Card
+              name={"Grins"}
+              idx={1}
+              line="m"
+              img={"https://i.ibb.co/6bS28bP/grins.jpg"}
+            />
+            <Card
+              name={"EBI"}
+              idx={2}
+              line="l"
+              data={[90, 80, 70, 90, 50]}
+              img={
+                "https://www.simplyrecipes.com/thmb/NOwXpq1nenarGiJnOTV7o5Oe_Aw=/1777x1333/smart/filters:no_upscale()/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2019__04__Beef-Pho-LEAD-2-afc6b6a9144947fb9d72070d7ea8c95c.jpg"
+              }
+            />
+            <Card name={"Kissam Kitchen"} idx={3} line="s" />
+            <Card name={"McTyeire"} idx={4} line="s" />
+            <Card name={"Commons"} idx={5} line="m" />
+            <Card name={"Rand"} idx={6} line="l" />
+            <Card name={"Zeppos"} idx={7} line="l" />
+
+            {/* <Card name={"Alumni Cafe"} idx={8} line="s" />
+            <Card name={"Holy Smokes"} idx={9} line="l" />
+            <Card name={"Local Java"} idx={10} line="s" />
+            <Card name={"Suzie's - Blair"} idx={11} line="m" />
+            <Card name={"Suzie's - FGH"} idx={12} line="l" />
+            <Card name={"Suzie's - MRB III"} idx={13} line="s" />
+            <Card name={"Suzie's - Central Library"} idx={14} line="m" /> */}
+          </Box>
+        </Box>
+      </ScrollView>
+    </View>
+  );
+}
+
 const styles = StyleSheet.create({
   baseView: {
     flex: 1,
@@ -34,56 +86,3 @@ const styles = StyleSheet.create({
     paddingBottom: 70,
   },
 });
-
-interface HomeScreenProps {
-  navigation?: Object;
-}
-
-export default function HomeScreen({ navigation }: HomeScreenProps) {
-  return (
-    <View style={styles.baseView}>
-      <ScrollView style={styles.scrollView}>
-        <Box style={styles.drawerContainer}>
-          <Box style={styles.drawerBox}>
-            {/* resident dining halls */}
-            <Card
-              name={"2301 Allergen Free"}
-              idx={0}
-              line="s"
-              data={[20, 35, 75, 10, 45]}
-              img={"https://i.ibb.co/p4kMnkZ/2301.png"}
-            />
-            <Card
-              name={"Grins"}
-              idx={1}
-              line="m"
-              img={"https://i.ibb.co/6bS28bP/grins.jpg"}
-            />
-            <Card
-              name={"EBI"}
-              idx={2}
-              line="l"
-              data={[90, 80, 70, 90, 50]}
-              img={
-                "https://www.simplyrecipes.com/thmb/NOwXpq1nenarGiJnOTV7o5Oe_Aw=/1777x1333/smart/filters:no_upscale()/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2019__04__Beef-Pho-LEAD-2-afc6b6a9144947fb9d72070d7ea8c95c.jpg"
-              }
-            />
-            {/* <Card name={"Kissam Kitchen"} idx={3} line="s" />
-            <Card name={"McTyeire"} idx={4} line="s" />
-            <Card name={"Commons"} idx={5} line="m" />
-            <Card name={"Rand"} idx={6} line="l" />
-            <Card name={"Zeppos"} idx={7} line="l" />
-
-            <Card name={"Alumni Cafe"} idx={8} line="s" />
-            <Card name={"Holy Smokes"} idx={9} line="l" />
-            <Card name={"Local Java"} idx={10} line="s" />
-            <Card name={"Suzie's - Blair"} idx={11} line="m" />
-            <Card name={"Suzie's - FGH"} idx={12} line="l" />
-            <Card name={"Suzie's - MRB III"} idx={13} line="s" />
-            <Card name={"Suzie's - Central Library"} idx={14} line="m" /> */}
-          </Box>
-        </Box>
-      </ScrollView>
-    </View>
-  );
-}
