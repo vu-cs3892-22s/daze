6a149b7f38e970fe4cf2f79ae6ac45441e1604de
Chinchuluun1029 <chinchuluun1029@gmail.com>
2022-04-08 12:59:53 -0500

    Add map w one marker

--- client/components/MapView.tsx
+++ client/components/MapView.tsx
@@ -1,7 +1,10 @@
 import React from "react";
-import { Button, View } from "react-native";
+import { Button, Dimensions, View } from "react-native";
+import Map, { Marker} from 'react-native-maps';
+
 import type { DefaultScreenNavigationProp } from "../types";
 
+const { width, height } = Dimensions.get("window");
 type NavigationProps = { navigation: DefaultScreenNavigationProp };
 
 export default function MapView({ navigation }: NavigationProps) {
@@ -21,9 +24,20 @@ export default function MapView({ navigation }: NavigationProps) {
 
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Button onPress={() => navigation.goBack()} title="Go back home" />
-
-      <Button onPress={() => signIn()} title="map" />
+      <Map 
+        initialRegion={{
+          latitude: 36.1455971,
+          longitude: -86.8042274,
+          latitudeDelta: 0.00922,
+          longitudeDelta: 0.00421,
+        }}
+        style={{ width: width, height: height }}
+      >
+        <Marker
+          coordinate={{ latitude : 36.14617723099077 , longitude : -86.8033166116821 }}
+          description="Rand"
+        />
+      </Map>
     </View>
   );
 }
