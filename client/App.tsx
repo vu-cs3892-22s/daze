bcf8260b679a03774f204b429a2fdaf298df5a8c
Sophia Chen <sophiachen@Sophias-MacBook-Pro-3.local>
2022-04-18 22:27:57 -0500

    Add in help modal

--- client/App.tsx
+++ client/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Button, NativeBaseProvider } from "native-base";
 import { NavigationContainer } from "@react-navigation/native";
 import { Ionicons, AntDesign, Feather } from "@expo/vector-icons";
@@ -14,12 +14,17 @@ import DefaultScreen from "./components/DefaultScreen";
 import MapView from "./components/MapView";
 import ListView from "./components/ListView";
 import DiningHall from "./components/DiningHall";
-import { Image } from "react-native";
+import { Image, Modal, View, StyleSheet, Text } from "react-native";
 import { TouchableWithoutFeedback } from "react-native-gesture-handler";
 
 const Tab = createBottomTabNavigator();
 
 WebBrowser.maybeCompleteAuthSession();
+
+interface lengthBody {
+  Color: String;
+  Explanation: String;
+}
 
 const attemptLogin = async (accessToken: string | undefined) => {
   // Should only be called when user tries to log in
@@ -39,7 +44,7 @@ const attemptLogin = async (accessToken: string | undefined) => {
   const secretKey = id + email; //PBKDF2(id + email, "daze-secret-key");
   // TODO: no hardcode
   try {
-    await fetch("https://cf93-129-59-122-20.ngrok.io/api/v1/user", {
+    await fetch("http://localhost:8080/api/v1/user", {
       method: "PUT",
       headers: {
         "Content-Type": "application/json",
@@ -68,9 +73,7 @@ export default function App() {
   // Get all locations
   const getAllLocations = async () => {
     try {
-      const response = await fetch(
-        "https://cf93-129-59-122-20.ngrok.io/api/v1/dining_halls"
-      );
+      const response = await fetch("http://localhost:8080/api/v1/dining_halls");
       const json = await response.json();
       const diningHalls = json.data;
 
@@ -89,6 +92,68 @@ export default function App() {
     email: string;
     secretKey: string;
   } | null>(null);
+
+  const [visible, setVisible] = useState(false);
+
+  const ModalPopup = ({ visible, children }) => {
+    return (
+      <Modal visible={visible} animationType="slide">
+        <View style={styles.modalBackground}>
+          <View style={[styles.modalContent]}>{children}</View>
+        </View>
+      </Modal>
+    );
+  };
+
+  const LineExplanation = () => {
+    return (
+      <View>
+        <View style={styles.LineExplanation}>
+          <View style={styles.waitTimeContainer}>
+            <View style={[styles.waitTimeBlob, { backgroundColor: "#B0DF63" }]}>
+              <Text style={styles.waitTimeMinute}>X</Text>
+              <Text>min</Text>
+            </View>
+          </View>
+          <View style={styles.textContainer}>
+            <Text>Short Line: &lt;5 mins</Text>
+          </View>
+        </View>
+        <View style={styles.LineExplanation}>
+          <View style={styles.waitTimeContainer}>
+            <View style={[styles.waitTimeBlob, { backgroundColor: "#FFFA76" }]}>
+              <Text style={styles.waitTimeMinute}>X</Text>
+              <Text>min</Text>
+            </View>
+          </View>
+          <View style={styles.textContainer}>
+            <Text>Medium Line: &lt;5 mins</Text>
+          </View>
+        </View>
+        <View style={styles.LineExplanation}>
+          <View style={styles.waitTimeContainer}>
+            <View style={[styles.waitTimeBlob, { backgroundColor: "#FF9B70" }]}>
+              <Text style={styles.waitTimeMinute}>X</Text>
+              <Text>min</Text>
+            </View>
+          </View>
+          <View style={styles.textContainer}>
+            <Text>Long Line: &lt;15 mins</Text>
+          </View>
+        </View>
+        <View style={styles.LineExplanation}>
+          <View style={styles.waitTimeContainer}>
+            <View
+              style={[styles.waitTimeBlob, { backgroundColor: "#CACACA" }]}
+            ></View>
+          </View>
+          <View style={styles.textContainer}>
+            <Text>Dining hall is closed</Text>
+          </View>
+        </View>
+      </View>
+    );
+  };
 
   useEffect(() => {
     AsyncStorage.getItem("userInfo").then((userInfo) => {
@@ -182,6 +247,15 @@ export default function App() {
                   style={{ paddingRight: 5 }}
                 />
               ),
+            headerLeft: () => (
+              <Ionicons
+                name="information-circle-outline"
+                size={24}
+                color="white"
+                onPress={() => setVisible(true)}
+                style={{ paddingLeft: 5 }}
+              />
+            ),
           })}
         >
           <Tab.Screen name="List View" component={ListView} />
@@ -193,6 +267,86 @@ export default function App() {
           />
         </Tab.Navigator>
       </NavigationContainer>
+      <ModalPopup visible={visible}>
+        <View>
+          <Ionicons
+            name="close-outline"
+            size={24}
+            color="black"
+            onPress={() => setVisible(false)}
+            style={{}}
+          />
+          <LineExplanation></LineExplanation>
+        </View>
+      </ModalPopup>
     </NativeBaseProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  LineExplanation: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "90%",
+    minHeight: 94,
+    backgroundColor: "#EFEFEF",
+    borderRadius: 25,
+    padding: 5,
+    marginBottom: 20,
+    marginLeft: 10,
+  },
+  textContainer: {
+    display: "flex",
+    flexGrow: 5,
+    marginLeft: 60,
+  },
+  waitTimeContainer: {
+    flex: 1,
+    flexDirection: "row",
+    flexGrow: 3,
+    marginLeft: 10,
+  },
+  waitTimeBlob: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 50,
+    height: 60,
+    width: 60,
+    padding: 10,
+  },
+  waitTimeMinute: {
+    fontSize: 24,
+    fontWeight: "700",
+  },
+  modalToggle: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 10,
+    borderWidth: 1,
+    borderColor: "#f2f2f2",
+    padding: 10,
+    borderRadius: 10,
+    alignSelf: "center",
+  },
+  modalClose: {
+    marginTop: 20,
+    marginBottom: 0,
+  },
+  modalBackground: {
+    flex: 1,
+    backgroundColor: "rgba(0,0,0,0.5)",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  modalContent: {
+    width: "80%",
+    backgroundColor: "white",
+    paddingHorizontal: 20,
+    paddingVertical: 30,
+    borderRadius: 10,
+    elevation: 10,
+  },
+});
